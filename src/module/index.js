7c05bcaf36eb3c91be9bea8d7a6d866addc71d89
Harlen Bright <plasmaui@gmail.com>
2019-11-24 07:47:46 +0200

    Link * Add custom props for Link * Add type checking for custom props in Link

--- src/module/index.js
+++ src/module/index.js
@@ -2,11 +2,16 @@ import React from "react";
 import Box from "../lib/Box";
 import Flexbox from "../lib/Flexbox";
 import Text from "../lib/Text";
+import Link from "../lib/Link";
 
 const App = () => (
-  <Flexbox justify="center" align="center">
-    <Text truncate>This is a long text that should be truncated.</Text>
-
+  <Flexbox dir="column" justify="center" align="center">
+    <Text truncate width="200px">
+      This is a long text that should be truncated.
+    </Text>
+    <Link href="https://plasma-ui.com" textDecoration="none" display="flex">
+      This is a link
+    </Link>
     <Box>Box 2</Box>
     <Box>Box 3</Box>
     <Box>Box 4</Box>
